5783a0f74100be1e1f8187ed961cf9d4e019cb7e
taaatang <taaatang.outlook.com>
2020-02-22 00:14:37 -0800

    bug fix

--- SpectraMain.cpp
+++ SpectraMain.cpp
@@ -133,7 +133,6 @@ for(int J2_num = 0; J2_num<1; J2_num++){
         std::vector<cdouble> ssvals;
         for (int i = 0; i < Lattice.getSiteNum(); i++){
             val = 0.0;
-            std::cout<<"befor SS gen..."<<std::endl
             SS.genPairMat(i);
             SS.MxV(gstate, vecTmp.data());
             vConjDotv<dataType, dataType>(gstate, vecTmp.data(), &val, SS.get_nloc());
