88161eb07c78b8cf08ab0f50b8c5101abd6e9ba7
taaatang <taaatang.outlook.com>
2020-03-02 23:18:46 -0800

    delete deprecated mkl func

--- SparseMatrix.hpp
+++ SparseMatrix.hpp
@@ -207,8 +207,7 @@ void SparseMatrix<T>::MxV(T *vecIn, T *vecOut){
             }
             // off-diagonal part
         #ifdef _MKL_
-            for (int i = 0; i < spmNum; i++){MKL::MxV2(A.at(i),vecBuf.data(),vecOut,parameters.at(i));}
-            // for (int i = 0; i < spmNum; i++){MKL::MxV(BaseMatrix<T>::nloc, BaseMatrix<T>::dim,parameters.at(i),valList.at(i),colList.at(i),rowInitList.at(i),vecBuf.data(),vecOut);}
+            for (int i = 0; i < spmNum; i++){MKL::MxV(A.at(i),vecBuf.data(),vecOut,parameters.at(i));}
         #else
             if (spmNum>0){
                 // constant sparse matrix
--- algebra.hpp
+++ algebra.hpp
@@ -25,12 +25,10 @@
 */
 namespace MKL{
     void diagTri(std::vector<double>* a, std::vector<double>* b, std::vector<double>* U);
-
-    void MxV(const MKL_INT& rows, const MKL_INT& cols, const MKL_Complex16& alpha, const std::vector<MKL_Complex16>& val, const std::vector<MKL_INT>& colIndx, const std::vector<MKL_INT>& rowIndx , const MKL_Complex16 *vecIn , MKL_Complex16 *vecOut);
-
+    
     void create(sparse_matrix_t& A, ind_int rows, ind_int cols, std::vector<MKL_INT>& rowInitList, std::vector<MKL_INT>& colList, std::vector<cdouble>& valList, MKL_INT mvNum=100);
     void destroy(sparse_matrix_t& A);
-    void MxV2(sparse_matrix_t& A, MKL_Complex16* vin, MKL_Complex16* vout, MKL_Complex16 alpha=1.0, MKL_Complex16 beta=1.0);
+    void MxV(sparse_matrix_t& A, MKL_Complex16* vin, MKL_Complex16* vout, MKL_Complex16 alpha=1.0, MKL_Complex16 beta=1.0);
 }
 
 
