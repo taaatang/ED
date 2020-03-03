5b1ffac1aa2e007f1ff57cd2061238a176b7c351
taaatang <taaatang.outlook.com>
2020-03-03 11:18:17 -0800

    add comment

--- algebra.hpp
+++ algebra.hpp
@@ -20,15 +20,19 @@
 #include "mkl.h"
 #include "omp.h"
 #include <vector>
-/*
-    LARPACK eigen solver for a real symmetric tridiagonal matrix
-*/
+
 namespace MKL{
+    /*
+        LARPACK eigen solver for a real symmetric tridiagonal matrix
+    */
     void diagTri(std::vector<double>* a, std::vector<double>* b, std::vector<double>* U);
-    
+    /*
+        inspector-executer mkl sparse.
+    */
     void create(sparse_matrix_t& A, ind_int rows, ind_int cols, std::vector<MKL_INT>& rowInitList, std::vector<MKL_INT>& colList, std::vector<cdouble>& valList, MKL_INT mvNum=100);
     void destroy(sparse_matrix_t& A);
     void MxV(sparse_matrix_t& A, MKL_Complex16* vin, MKL_Complex16* vout, MKL_Complex16 alpha=1.0, MKL_Complex16 beta=1.0);
+    
 }
 
 
