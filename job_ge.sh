cae5915a7fc6c93aebb026ec61179c8899d6a0eb
taaatang <taaatang.outlook.com>
2020-05-06 23:44:03 -0700

    bug fix

--- job_ge.sh
+++ job_ge.sh
@@ -9,18 +9,18 @@ cd ${cwd}
 
 JobDir=Job/sq8_4u4d/Basis
 keyword=kIndex
-app=build/genBasis.out
+app=genBasis.out
 script=run_haswell.sh
-input=Input/symm_input.txt
+input=symm_input.txt
 for i in 0 1 2 3 4 5 6 7
     do
         dir=${JobDir}/k${i}
-        appDir=dir/App
-        inputDir=dir/Input
+        appDir=${dir}/App
+        inputDir=${dir}/Input
         mkdir -p ${appDir}
         mkdir -p ${inputDir}
-        cp ${app}  ${appDir}/${app}
-        sed -e "s/${keyword}/${i}/g" ${input} > ${inputDir}/${input}
+        cp build/${app}  ${appDir}/${app}
+        sed -e "s/${keyword}/${i}/g" Input/${input} > ${inputDir}/${input}
         sed -e "s/${keyword}/${i}/g" ${script} > ${dir}/${script}
         cd ${dir}
         rm -rf job.out
