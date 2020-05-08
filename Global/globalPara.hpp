55b0b48181aeed5883c1f6403ba19633283cfe7b
taaatang <taaatang.outlook.com>
2020-05-07 21:08:16 -0700

    change root data directary

--- Global/globalPara.hpp
+++ Global/globalPara.hpp
@@ -80,7 +80,7 @@ const int MPI_MASTER = 0;
     *****************
 */
 // cori
-const std::string ROOT_DATA_PATH = "/global/project/projectdirs/m2757/tatang/test"; 
+const std::string ROOT_DATA_PATH = "/global/project/projectdirs/m2757/tatang"; 
 // sherlock
 // const std::string ROOT_DATA_PATH = "/oak/stanford/groups/tpd/tatang"; 
 const std::string PROJECT_DATA_PATH = ROOT_DATA_PATH + "/Photodoping";
