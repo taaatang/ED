f766d3e5450a56e4ce714e4143b22bd90817f3a2
taaatang <taaatang.outlook.com>
2020-05-14 10:17:53 -0700

    GaussPulse

---
+++ Utils/pulse.cpp
@@ -0,0 +1,66 @@
+#include "pulse.hpp"
+
+Pulse::pulse(double w_, double width = 10.0, double dt_ = 0.01, int numSteps_ = 15000):{
+    tu = 1.05457/1.60218; // in unit of fs
+    Eu = 0.160218/3/1.055; // in unit of 10^8 V/m
+    Au = Eu*tu*1e-7; // in unit of V/m*s
+
+    params.resize(4);
+    params.at(0) = 10.0/Eu; // default E0 10*10^8 V/m = 0.1V/am
+    params.at(1) = width * std::sqrt(2)/tu;
+    params.at(2) = w_;
+    params.at(3) = PI/2.0;
+    dt = dt_;
+    numSteps = numSteps_;
+
+    tc = dt*numSteps/2.0;
+    ti = 0.0-tc;
+    tf = dt*numSteps-tc;
+
+    FuncE.function = &GaussPulse;
+    FuncE.params = params.data();
+
+    FuncE2.function = &GaussPulse2;
+    FuncE2.params = params.data();
+    epsabs = 1e-10;
+    epsrel = 1e-6;
+
+    Fluence = computeFluence();
+
+}
+
+double Pulse::getE(int stepIdx){
+    double t = stepIdx*dt-tc;
+    return GaussPulse(t,params.data());
+}
+
+double Pulse::getA(int stepIdx){
+    double t = stepIdx*dt-tc;
+    double result, abserr;
+    size_t neval;
+    gsl_integration_qng(&FuncE, ti, t, epsabs, epsrel, &result, &abserr, &neval);
+    return result;
+}
+
+double Pulse::computeFluence(){
+    double result, abserr;
+    size_t neval;
+    gsl_integration_qng(&FuncE2, ti, tf, epsabs, epsrel, &result, &abserr, &neval);
+    return 3.0*8.85*tu*Eu*Eu*result*1e-7;
+}
+
+void Pulse::setFluence(double Flu){
+    params.at(0) *= std::sqrt(Flu/Fluence); 
+    double Flu_ckeck = computeFluence();
+    assert_msg(std::abs(Flu-Flu_check)<1e-6,"Err bigger than 1e-6 in Pulse::setFluence!");
+    Fluence = Flu;
+}
+
+void Pulse::print() const {
+    std::cout<<"Pulse Info:\n";
+    std::cout<<"E0:"<<params.at(0)*Eu<<"*10^8 V/m\n"\
+             <<"sigma:"<<params.at(1)*tu<<"fs\n"\
+             <<"w:"<<params.at(2)<<"eV\n"\
+             <<"phase:"<<params.at(3)<<"\n"\
+             <<"Fluence:"<<Fluence<<"mJ/cm^2\n";
+}
---
+++ Utils/pulse.hpp
@@ -0,0 +1,69 @@
+//
+//  pulse.hpp
+//  ED
+//
+//  Created by tatang on 10/27/19.
+//  Copyright © 2019 tatang. All rights reserved.
+//
+
+#ifndef pulse_hpp
+#define pulse_hpp
+
+#include "../Global/globalPara.hpp"
+#include "../Utils/utils.hpp"
+#include <gsl/gsl_integration.h>
+#include <cmath>
+
+double GaussPulse(double t, void* params){
+    double *Params = (double *) params;
+    double amp = Params[0], sigma = Params[1], freq = Params[2], phase = Params[3];
+    return amp*std::exp(-t*t/sigma/sigma)*std::sin(freq*t+phase);
+}
+
+double GaussPulse2(double t, void* params){
+    double E = GaussPulse(t, params);
+    return E*E;
+}
+
+class Pulse{
+/*
+    Gaussian shape elextric field with frequency w.
+    return vector potential A(t) = \int_{ti}^{t} E(t).
+    unit: hbar = 1, eV = 1, c = 1, e = 1
+          tu = hbar/eV, Lu = c*tu, Eu = eV/e/Lu
+*/
+public:
+    pulse(double w_/*eV*/, double width = 10.0/*fs*/, double dt_ = 0.01/*tu*/, int numSteps_ = 15000);
+    ~pulse(){};
+
+    double getE(int stepIdx) const;
+    double getFluence() const {return Fluence;}
+    double computeFluence() const;
+    double getA(int stepIdx) const;
+
+    setE0(double E0){params.at(0)=E0;}
+    setWidth(double width){params.at(1)=width*std::sqrt(2);}
+    setW(double w){params.at(2)=w;}
+    setPhase(double phase){params.at(3)=phase;}
+    void setFluence(double Flu /* mJ/cm^2 */);
+
+    void print() const;
+
+private:
+    double E, A;
+
+    double Eu, Au; // electric field unit
+    std::vector<double> params; // params = {E0, sigma, w, phase}, sigma = sqrt(2)*width, where I(width) = I(0)/e
+    double Fluence;
+
+    double dt;
+    int numSteps;
+    double tu; // time unit: hbar/eV, fs
+    double ti,tc,tf; 
+
+    gsl_function FuncE;
+    gsl_function FuncE2;
+    double epsabs, epsrel;
+};
+
+#endif // pulse_hpp
