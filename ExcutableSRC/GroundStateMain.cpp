8691a069f222f92d80abe5032356c96bd8762ea6
taaatang <taaatang@outlook.com>
2020-06-05 08:36:22 -0700

    test tJ point group

--- ExcutableSRC/GroundStateMain.cpp
+++ ExcutableSRC/GroundStateMain.cpp
@@ -30,7 +30,7 @@ int main(int argc, const char * argv[]) {
     * Input And Initialization *
     ****************************
 */
-    LATTICE_MODEL model = LATTICE_MODEL::t_J;
+    LATTICE_MODEL model = LATTICE_MODEL::HEISENBERG;
     int Nx, Ny, N, Nu, Nd;
     int kIndex = -1; // Gamma Point
     int PGRepIndex = -1;
@@ -109,10 +109,10 @@ int main(int argc, const char * argv[]) {
         J1Link.addLinkVec(VecD{1.0,0.0,0.0}).addLinkVec(VecD{0.0,1.0,0.0}).addLinkVec(VecD{1.0,-1.0,0.0});
         J2Link.addLinkVec(VecD{1.0,1.0,0.0}).addLinkVec(VecD{-1.0,2.0,0.0}).addLinkVec(VecD{2.0,-1.0,0.0});
         timer.tik();
-        HtJ<dataType> H(&Lattice, &B, 1);
-        H.pushLinks({t1Link, t2Link, J1Link, J2Link});
-        // Heisenberg<dataType> H(&Lattice, &B, 1);
-        // H.pushLinks({J1Link, J2Link});
+        // HtJ<dataType> H(&Lattice, &B, 1);
+        // H.pushLinks({t1Link, t2Link, J1Link, J2Link});
+        Heisenberg<dataType> H(&Lattice, &B, 1);
+        H.pushLinks({J1Link, J2Link});
         H.genMatPara(rowPerThread);
         timer.tok();
         /*
