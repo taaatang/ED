ec6c910ae270a97a09702068536e609a04ea438e
taaatang <taaatang@outlook.com>
2020-06-04 17:13:17 -0700

    test HtJ

--- ExcutableSRC/GroundStateMain.cpp
+++ ExcutableSRC/GroundStateMain.cpp
@@ -30,7 +30,7 @@ int main(int argc, const char * argv[]) {
     * Input And Initialization *
     ****************************
 */
-    LATTICE_MODEL model = LATTICE_MODEL::t_J;
+    LATTICE_MODEL model = LATTICE_MODEL::HEISENBERG;
     int Nx, Ny, N, Nu, Nd;
     int kIndex = -1; // Gamma Point
     int PGRepIndex = -1;
@@ -104,11 +104,15 @@ int main(int argc, const char * argv[]) {
         Link<dataType> t2Link(LINK_TYPE::HOPPING_T, {ORBITAL::SINGLE, ORBITAL::SINGLE}, 1.0);
         Link<dataType> J1Link(LINK_TYPE::SUPER_EXCHANGE_J, {ORBITAL::SINGLE, ORBITAL::SINGLE}, 1.0);
         Link<dataType> J2Link(LINK_TYPE::SUPER_EXCHANGE_J, {ORBITAL::SINGLE, ORBITAL::SINGLE}, 1.0, true);
+        t1Link.addLinkVec(VecD{1.0,0.0,0.0}).addLinkVec(VecD{0.0,1.0,0.0}).addLinkVec(VecD{1.0,-1.0,0.0});
+        t2Link.addLinkVec(VecD{1.0,1.0,0.0}).addLinkVec(VecD{-1.0,2.0,0.0}).addLinkVec(VecD{2.0,-1.0,0.0});
         J1Link.addLinkVec(VecD{1.0,0.0,0.0}).addLinkVec(VecD{0.0,1.0,0.0}).addLinkVec(VecD{1.0,-1.0,0.0});
         J2Link.addLinkVec(VecD{1.0,1.0,0.0}).addLinkVec(VecD{-1.0,2.0,0.0}).addLinkVec(VecD{2.0,-1.0,0.0});
         timer.tik();
-        HtJ<dataType> H(&Lattice, &B, 1);
-        H.pushLinks({t1Link, t2Link, J1Link, J2Link});
+        // HtJ<dataType> H(&Lattice, &B, 1);
+        // H.pushLinks({t1Link, t2Link, J1Link, J2Link});
+        Heisenberg<dataType> H(&Lattice, &B, 1);
+        H.pushLinks({J1Link, J2Link});
         H.genMatPara(rowPerThread);
         timer.tok();
         /*
