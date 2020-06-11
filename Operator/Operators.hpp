3e2ad362d53c43e526d55fd4b0f64d05ff13c01b
taaatang <taaatang.outlook.com>
2020-06-10 21:34:51 -0700

    typo

--- Operator/Operators.hpp
+++ Operator/Operators.hpp
@@ -223,7 +223,7 @@ public:
     void setplz(VecD pIn_, VecD pOut_);
     void setVal(int matID, double val){(SparseMatrix<T>::parameters).at(matID) = val;}
     void row(ind_int rowID, std::vector<MAP>& rowMaps);
-}
+};
 
 
 template <class T>
