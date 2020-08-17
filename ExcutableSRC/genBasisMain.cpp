8ec81b8840a93d3e12c3e7008c261c530ce25476
taaatang <taaatang.outlook.com>
2020-08-17 11:15:23 -0700

    N=36 gen Basis

--- ExcutableSRC/genBasisMain.cpp
+++ ExcutableSRC/genBasisMain.cpp
@@ -38,18 +38,19 @@ int main(int argc, const char * argv[]) {
     // N = Nx * Ny;
     infile>>kIndex;
     infile.close(); 
-    N=8, Nu=4, Nd=4;
+    N=36, Nu=18, Nd=18;
     
-    // std::string subDir = "/"+std::to_string(N);
+    std::string subDir = "/"+std::to_string(N);
     // std::string subDir = "/"+std::to_string(Nx)+"by"+std::to_string(Ny)+"_"+std::to_string(Nu)+"u"+std::to_string(Nd)+"d";
-    std::string subDir = "/sq"+std::to_string(N)+"_"+std::to_string(Nu)+"u"+std::to_string(Nd)+"d";
+    // std::string subDir = "/sq"+std::to_string(N)+"_"+std::to_string(Nu)+"u"+std::to_string(Nd)+"d";
     Timer timer;
     
-    // if (workerNum > 1){
-    //     kIndex = workerID;
-    // }
+    if (workerNum > 1){
+        kIndex = workerID;
+    }
 
-    for(kIndex=0; kIndex<N; kIndex++){
+    // for(kIndex=0; kIndex<N; kIndex++){
+    if(kIndex < N){
         // create data path
         std::string dataDir = PROJECT_DATA_PATH+ subDir +"/kSpace/Basis/"+std::to_string(kIndex);
         system(("mkdir -p " + dataDir).c_str());
@@ -61,46 +62,47 @@ int main(int argc, const char * argv[]) {
         * Lattice and Basis Initialization *
         ************************************
     */
-        SquareLattice Lattice(N);
-        Lattice.addOrb({ORBITAL::Dx2y2,0,{0.0,0.0,0.0}}).addOrb({ORBITAL::Px,1,{0.5,0.0,0.0}}).addOrb({ORBITAL::Py,2,{0.0,0.5,0.0}});
-        Lattice.addOrb({ORBITAL::Pzu,3,{0.0,0.0,0.5}}).addOrb({ORBITAL::Pzd,4,{0.0,0.0,-0.5}});
-        // TriAngLattice Lattice(N);
-        // Lattice.addOrb({});
+        // SquareLattice Lattice(N);
+        // Lattice.addOrb({ORBITAL::Dx2y2,0,{0.0,0.0,0.0}}).addOrb({ORBITAL::Px,1,{0.5,0.0,0.0}}).addOrb({ORBITAL::Py,2,{0.0,0.5,0.0}});
+        // Lattice.addOrb({ORBITAL::Pzu,3,{0.0,0.0,0.5}}).addOrb({ORBITAL::Pzd,4,{0.0,0.0,-0.5}});
+        TriAngLattice Lattice(N);
+        Lattice.addOrb({});
         Lattice.construct();
         int siteDim = 2;
         VecI occList{Nu, Nd};
     
-        // timer.tik();
-        // Basis B(LATTICE_MODEL::HUBBARD, &Lattice, occList, kIndex);
-        // B.gen();
-        // timer.tok();
-        // std::cout<<"WorkerID:"<<workerID<<", kInd ="<<B.getkIndex()<<", size="<<B.getSubDim()<<"/"<<B.getTotDim()<<". Basis construction time:"<<timer.elapse()<<" milliseconds."<<std::endl;
-        // B.saveBasis(basisfile, normfile);
-
-        //Paralel Construction
         timer.tik();
         Basis B(LATTICE_MODEL::HUBBARD, &Lattice, occList, kIndex);
-        B.gen(workerID,workerNum);
+        B.gen();
         timer.tok();
-        std::cout<<"WorkerID:"<<workerID<<", kInd ="<<B.getkIndex()<<", local size="<<B.getLocDim()<<"/"<<B.getTotDim()<<". Basis construction time:"<<timer.elapse()<<" milliseconds."<<std::endl;
-        int msg = 1;
-        bool is_app = true;
-        if(workerID==0){
-            B.saveBasis(basisfile, normfile);
-            std::cout<<"workerID:"<<workerID<<", data saved!"<<std::endl;
-            if(workerNum>1){
-                MPI_Send(&msg,1,workerID+1);
-            }
-        }else if((workerID>0) and (workerID<(workerNum-1))){
-            MPI_Recv(&msg,1,workerID-1);
-            B.saveBasis(basisfile, normfile, is_app);
-            std::cout<<"workerID:"<<workerID<<", data saved!"<<std::endl;
-            MPI_Send(&msg,1,workerID+1);
-        }else if(workerID==(workerNum-1)){
-            MPI_Recv(&msg,1,workerID-1);
-            B.saveBasis(basisfile, normfile, is_app);
-            std::cout<<"workerID:"<<workerID<<", data saved!"<<std::endl;
-        }
+        std::cout<<"WorkerID:"<<workerID<<", kInd ="<<B.getkIndex()<<", size="<<B.getSubDim()<<"/"<<B.getTotDim()<<". Basis construction time:"<<timer.elapse()<<" milliseconds."<<std::endl;
+        B.saveBasis(basisfile, normfile);
+
+        //Paralel Construction
+        // timer.tik();
+        // Basis B(LATTICE_MODEL::HUBBARD, &Lattice, occList, kIndex);
+        // B.gen(workerID,workerNum);
+        // timer.tok();
+        // std::cout<<"WorkerID:"<<workerID<<", kInd ="<<B.getkIndex()<<", local size="<<B.getLocDim()<<"/"<<B.getTotDim()<<". Basis construction time:"<<timer.elapse()<<" milliseconds."<<std::endl;
+        // int msg = 1;
+        // bool is_app = true;
+        // if(workerID==0){
+        //     B.saveBasis(basisfile, normfile);
+        //     std::cout<<"workerID:"<<workerID<<", data saved!"<<std::endl;
+        //     if(workerNum>1){
+        //         MPI_Send(&msg,1,workerID+1);
+        //     }
+        // }else if((workerID>0) and (workerID<(workerNum-1))){
+        //     MPI_Recv(&msg,1,workerID-1);
+        //     B.saveBasis(basisfile, normfile, is_app);
+        //     std::cout<<"workerID:"<<workerID<<", data saved!"<<std::endl;
+        //     MPI_Send(&msg,1,workerID+1);
+        // }else if(workerID==(workerNum-1)){
+        //     MPI_Recv(&msg,1,workerID-1);
+        //     B.saveBasis(basisfile, normfile, is_app);
+        //     std::cout<<"workerID:"<<workerID<<", data saved!"<<std::endl;
+        // }
+
         // check
         // std::cout<<"WorkerID"<<workerID<<", begin reading basis..."<<std::endl;
         // tic = std::chrono::system_clock::now();
