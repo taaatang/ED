e7cd7d7d5832624436ff0dbaf85389ebf78e0373
taaatang <taaatang.outlook.com>
2020-08-17 04:04:24 -0700

    job ge sh

--- job_ge.sh
+++ job_ge.sh
@@ -14,20 +14,21 @@ app=Raman.out
 script=run_haswell.sh
 input=params_input.txt
 for i in 0 15 30
-    for j in 0 1 2 3 10 20 30 40 
-        do
-            dir=${JobDir}/J2_${i}_Jk_${j}
-            appDir=${dir}/App
-            inputDir=${dir}/Input
-            mkdir -p ${appDir}
-            mkdir -p ${inputDir}
-            cp build/${app}  ${appDir}/${app}
-            sed -e "s/${keyword1}/${i}/g;s/${keyword2}/${j}/g" Input/${input} > ${inputDir}/${input}
-            sed -e "s/${keyword1}/${i}/g;s/${keyword2}/${j}/g;s/APP/${app}/g" ${script} > ${appDir}/${script}
-            cd ${appDir}
-            rm -rf job.out
-            rm -rf job.err
-            sbatch ${script}
-            cd ${cwd}
-        done
+    do
+        for j in 0 1 2 3 10 20 30 40 
+            do
+                dir=${JobDir}/J2_${i}_Jk_${j}
+                appDir=${dir}/App
+                inputDir=${dir}/Input
+                mkdir -p ${appDir}
+                mkdir -p ${inputDir}
+                cp build/${app}  ${appDir}/${app}
+                sed -e "s/${keyword1}/${i}/g;s/${keyword2}/${j}/g" Input/${input} > ${inputDir}/${input}
+                sed -e "s/${keyword1}/${i}/g;s/${keyword2}/${j}/g;s/APP/${app}/g" ${script} > ${appDir}/${script}
+                cd ${appDir}
+                rm -rf job.out
+                rm -rf job.err
+                sbatch ${script}
+                cd ${cwd}
+            done
     done
