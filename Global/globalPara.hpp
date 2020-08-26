618f225b2f7819483f46c00d9c11e824d0cb3065
taaatang <taaatang.outlook.com>
2020-08-26 09:51:10 -0700

    acer data path

--- Global/globalPara.hpp
+++ Global/globalPara.hpp
@@ -80,11 +80,11 @@ const int MPI_MASTER = 0;
     *****************
 */
 // cori
-const std::string ROOT_DATA_PATH = "/global/project/projectdirs/m2757/tatang"; 
+// const std::string ROOT_DATA_PATH = "/global/project/projectdirs/m2757/tatang";  
 // sherlock
 // const std::string ROOT_DATA_PATH = "/oak/stanford/groups/tpd/tatang"; 
 // acer
-// const std::string ROOT_DATA_PATH = "~/Project/DATA"; 
+const std::string ROOT_DATA_PATH = "/home/tatang/Project/DATA"; 
 
 // const std::string PROJECT_DATA_PATH = ROOT_DATA_PATH + "/Photodoping";
 // const std::string PROJECT_DATA_PATH = ROOT_DATA_PATH + "/TriAngtJ";
