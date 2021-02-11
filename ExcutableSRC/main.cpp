a0592f89feb2dca194451e74489c70be175ff94b
taaatang <taaatang@outlook.com>
2021-02-11 02:12:12 -0800

    4x1 pump_x

--- ExcutableSRC/main.cpp
+++ ExcutableSRC/main.cpp
@@ -22,33 +22,33 @@ int main(){
     PDiag.diag();
     cdouble* gstate = PDiag.getEigvec();
 
-    // setpulse(pulsePara, pulse);
-    // std::string TimePath = PROJECT_DATA_PATH + "/sqOcta/2x2/3u2d/k1/pump/x/" + tostr(pulse.getw());
-    // mkdir_fs(TimePath);
-    // H->setPeierls(&pulse);
-    // H->construct();
-    // TimeEvolver<cdouble> Tevol(gstate, H.get(), 15);
-    // Nocc occ(latt.get(), Bi.get()); 
-    // occ.genMat();
-    // VecD dx2y2,px,py,pzu,pzd;
-    // while (H->next()) {
-    //     Tevol.evolve(-0.01);
-    //     dx2y2.push_back(occ.count(ORBITAL::Dx2y2, Tevol.getVec()));
-    //     px.push_back(occ.count(ORBITAL::Px,Tevol.getVec()));
-    //     py.push_back(occ.count(ORBITAL::Py,Tevol.getVec()));
-    //     pzu.push_back(occ.count(ORBITAL::Pzu,Tevol.getVec()));
-    //     pzd.push_back(occ.count(ORBITAL::Pzd,Tevol.getVec()));
-    // }
+    setpulse(pulsePara, pulse);
+    std::string TimePath = PROJECT_DATA_PATH + "/sqOcta/4x1/2u2d/k2/pump/x/" + tostr(pulse.getw());
+    mkdir_fs(TimePath);
+    H->setPeierls(&pulse);
+    H->construct();
+    TimeEvolver<cdouble> Tevol(gstate, H.get(), 15);
+    Nocc occ(latt.get(), Bi.get()); 
+    occ.genMat();
+    VecD dx2y2,px,py,pzu,pzd;
+    while (H->next()) {
+        Tevol.evolve(-0.01);
+        dx2y2.push_back(occ.count(ORBITAL::Dx2y2, Tevol.getVec()));
+        px.push_back(occ.count(ORBITAL::Px,Tevol.getVec()));
+        py.push_back(occ.count(ORBITAL::Py,Tevol.getVec()));
+        pzu.push_back(occ.count(ORBITAL::Pzu,Tevol.getVec()));
+        pzd.push_back(occ.count(ORBITAL::Pzd,Tevol.getVec()));
+    }
 
 
-    // if (workerID == MPI_MASTER) {
-    //     std::ofstream outfile;
-    //     save<double>(dx2y2.data(),(int)dx2y2.size(),&outfile,TimePath+"/dx2y2");
-    //     save<double>(px.data(),(int)px.size(),&outfile,TimePath+"/px");
-    //     save<double>(py.data(),(int)py.size(),&outfile,TimePath+"/py");
-    //     save<double>(pzu.data(),(int)pzu.size(),&outfile,TimePath+"/pzu");
-    //     save<double>(pzd.data(),(int)pzd.size(),&outfile,TimePath+"/pzd");
-    // }
+    if (workerID == MPI_MASTER) {
+        std::ofstream outfile;
+        save<double>(dx2y2.data(),(int)dx2y2.size(),&outfile,TimePath+"/dx2y2");
+        save<double>(px.data(),(int)px.size(),&outfile,TimePath+"/px");
+        save<double>(py.data(),(int)py.size(),&outfile,TimePath+"/py");
+        save<double>(pzu.data(),(int)pzu.size(),&outfile,TimePath+"/pzu");
+        save<double>(pzd.data(),(int)pzd.size(),&outfile,TimePath+"/pzd");
+    }
     MPI_Finalize();
     return 0;
 }
