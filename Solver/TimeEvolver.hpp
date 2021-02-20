1fa8daa8620f7f54270a91dbf1c860a41a02b14e
taaatang <taaatang@outlook.com>
2021-02-20 08:57:13 -0800

    clean TimeEvolver code

--- Solver/TimeEvolver.hpp
+++ Solver/TimeEvolver.hpp
@@ -25,19 +25,26 @@
 */
 template <class T>
 class TimeEvolver: public LANCZOSIterator<T>{
+public:
+    TimeEvolver( ) { }
+    TimeEvolver(T* initVec, BaseMatrix<dataType> *M, int krydim);
+    ~TimeEvolver( ) { }
+
+    T* getVec( ) { return vec; }
+    // time evolve one step
+    void evolve(double expFac);
+
 private:
     T* vec;
     std::vector<cdouble> tmp1, tmp2;
     std::vector<double> U;
-public:
-    TimeEvolver(){};
-    TimeEvolver(T* initVec, BaseMatrix<dataType> *M, int krydim):LANCZOSIterator<T>(M,krydim,ALPHA_BETA_Q),vec(initVec),tmp1(krydim),tmp2(krydim),U(krydim*krydim){};
-    ~TimeEvolver(){};
-
-    T* getVec(){return vec;}
-    // time evolve one step
-    void evolve(double expFac);
 };
+
+template <class T>
+TimeEvolver<T>::TimeEvolver(T* initVec, BaseMatrix<dataType> *M, int krydim):LANCZOSIterator<T>(M, krydim, ALPHA_BETA_Q), \
+vec(initVec), tmp1(krydim), tmp2(krydim), U(krydim * krydim) {
+
+}
 
 // vec = exp(i*expFac*H)*vec
 template <class T>
@@ -46,20 +53,20 @@ void TimeEvolver<T>::evolve(double expFac){
     // Tri = Z*Diag*Z^
     MKL::diagTri(&(LANCZOSIterator<T>::alpha), &(LANCZOSIterator<T>::beta), &U);
     #pragma omp parallel for
-    for (int i = 0; i < LANCZOSIterator<T>::krylovDim; ++i){
+    for (int i = 0; i < LANCZOSIterator<T>::krylovDim; ++i) {
         tmp1[i] = std::exp(-CPLX_I*expFac*LANCZOSIterator<T>::alpha.at(i))*U[i*LANCZOSIterator<T>::krylovDim];
         tmp2[i] = 0.0;
     } 
     #pragma omp parallel for
-    for (int i = 0; i < LANCZOSIterator<T>::krylovDim; ++i){
-        for (int j = 0; j < LANCZOSIterator<T>::krylovDim; j++){
+    for (int i = 0; i < LANCZOSIterator<T>::krylovDim; ++i) {
+        for (int j = 0; j < LANCZOSIterator<T>::krylovDim; ++j){
             tmp2[i] += U[i+j*LANCZOSIterator<T>::krylovDim] * tmp1[j];
         }
     }
     #pragma omp parallel for
-    for (int i = 0; i < LANCZOSIterator<T>::M_->getnloc(); ++i){
+    for (int i = 0; i < LANCZOSIterator<T>::M_->getnloc(); ++i) {
         cdouble sum = 0.0;
-        for(int j = 0; j < LANCZOSIterator<T>::krylovDim; j++){
+        for(int j = 0; j < LANCZOSIterator<T>::krylovDim; ++j){
             sum += LANCZOSIterator<T>::Q[i + j *LANCZOSIterator<T>::M_->getnloc()] * tmp2[j];
         }
         vec[i] = sum;
