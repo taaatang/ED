6ff9db07f6d85be954e5de6cda0bb5fabdc57c60
taaatang <taaatang@outlook.com>
2021-02-20 00:45:58 -0800

    mkl blas 1

--- Algebra/algebra.hpp
+++ Algebra/algebra.hpp
@@ -37,6 +37,48 @@ namespace MKL{
     void MxV(sparse_matrix_t& A, double* vin, double* vout, double alpha=1.0, double beta=1.0);
 
     void destroy(sparse_matrix_t& A);
+
+    // BLAS 1
+    // y = a * x + y
+    void axpy(MKL_INT n, const double a, const double *x, double *y) {
+        cblas_daxpy(n, a, x, 1, y, 1);
+    }
+    void axpy(const MKL_INT n, const cdouble a, const cdouble *x, cdouble *y) {
+        cblas_zaxpy(n, a, x, 1, y,1);
+    }
+    // y = x
+    void copy(const MKL_INT n, const double *x, double *y) {
+        cblas_dcopy(n, x, 1, y, 1);
+    }
+    void copy(const MKL_INT n, const cdouble *x, cdouble *y) {
+        cblas_zcopy(n, x, 1, y, 1);
+    }
+    // conj(x).dot(y)
+    double dot(const MKL_INT n, const double *x, const double *y) {
+        cblas_ddot(n, x, 1, y, 1);
+    }
+    cdouble dot(const MKL_INT n, const cdouble *x, const cdouble *y) {
+        cdouble res = 0.0;
+        cblas_zdotc_sub(n, x, 1, y, q, &res);
+        return res;
+    }
+    // Euclidean norm
+    double norm(const MKL_INT n, const double *x){
+        cblas_dnrm2(n, x, 1);
+    }
+    double norm(const MKL_INT n, const cdouble *x){
+        cblas_dznrm2(n, x, 1);
+    }
+    // x = a * x
+    void scale(const MKL_INT n, const double a, double *x) {
+        cblas_dscal(n, a, x, 1);
+    }
+    void scale(const MKL_INT n, const cdouble a, cdouble *x) {
+        cblas_zscal(n, a, x, 1);
+    }
+    void scale(const MKL_INT n, const double a, cdouble *x) {
+        cblas_zdscal(n, a, x, 1);
+    }
 }
 
 
