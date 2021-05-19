d8e897128c95137f1814dd9393ac7f4eb8ba09a0
taaatang <taaatang@outlook.com>
2021-05-18 20:58:46 -0700

    para get function

--- ExcutableSRC/heisenberg.cpp
+++ ExcutableSRC/heisenberg.cpp
@@ -5,12 +5,11 @@
 #include "Solver/PARPACKSolver.hpp"
 #include "Solver/Spectra.hpp"
 
-using namespace std;
-
 int main( ) {
 
     MPI_Init(NULL, NULL);
-    int workerID, workerNum;
+    int workerID{0}; 
+    int workerNum{0};
     mpi_info(workerID, workerNum);
     bool isMaster = (workerID == MPI_MASTER);
     Timer timer(isMaster);
@@ -26,18 +25,18 @@ int main( ) {
     MPI_Barrier(MPI_COMM_WORLD);
 
     // compute and save basis with translation symm
-    if (measure("basis") && !para.geti("basis")) {
+    if (measure("basis") && !*para.get<bool>("basis")) {
         setlatt(para, latt);
         for (int kidx = workerID; kidx < latt->getSiteNum(); kidx += workerNum) {
-            Basis b(para.getmodel(), latt.get(), {para.geti("nu"), para.geti("nd")}, kidx);
+            Basis b(para.getmodel(), latt.get(), {*para.get<int>("nu"), *para.get<int>("nd")}, kidx);
             b.construct();
             b.save(path.getBasisDir(kidx));
         }
     }
 
     // eigenstate
-    std::vector<dataType> w;
-    std::vector<dataType*> state;
+    std::vector<dataType> w{};
+    std::vector<dataType*> state{};
     int nstate = 0;
     if (measure("state")) {
         timer.tik();
@@ -51,17 +50,21 @@ int main( ) {
             H->print("Hamiltonian from master worker");
         }
         // cout<<setprecision(10);
-        int nev = measurePara.geti("nev");
+        auto nev = *measurePara.get<int>("nev");
         H->diag(nev);
         for (int n = 0; n < nev; ++n) {
             w.push_back(H->getEval(n));
             state.push_back(H->getEvec(n));
         }
-        nstate = sort(w, state, measure("ground state"));
-        if (isMaster) save(w.data(), (int)(w.size()), path.evalFile);
-        if (isMaster) cout<<"sorted evals:"<<w<<"\n";
+        nstate = sort(w, state, measure("zeroTmp"));
+        if (isMaster) {
+            save(w.data(), (int)(w.size()), path.evalFile);
+        }
+        if (isMaster) {
+            std::cout << "sorted evals:" << w << '\n';
+        }
     }
-    int krylovDim = measurePara.geti("krylovDim");
+    int krylovDim = *measurePara.get<int>("krylovDim");
     // spin spin correlation
     if (measure("SiSj")) {
         timer.tik();
@@ -73,7 +76,7 @@ int main( ) {
             SS.construct();
             for (int n = 0; n < nstate; ++n) {
                 auto val = SS.vMv(state[n], state[n]) / dataType(latt->getSiteNum());
-                // if (isMaster) std::cout<<"ss(r="<<i<<")="<<val<<std::endl;
+                if (isMaster) std::cout<<"ss(r="<<i<<")="<<val<<std::endl;
                 ssvals[n].push_back(val);
             }
         }
@@ -106,8 +109,8 @@ int main( ) {
     // Raman spectra
     if (measure("Raman")) {
         timer.tik();
-        auto J1 = para.getd("J1");
-        auto J2 = para.getd("J2");
+        auto J1 = *para.get<double>("J1");
+        auto J2 = *para.get<double>("J2");
         for (auto channel : std::vector<std::string> {"A1", "A2", "E21", "E22"}) {
             Hamiltonian<LATTICE_MODEL::HEISENBERG, dataType> R(latt.get(), Bi.get(), Bi.get());
             R.pushLinks(RamanChannel(channel, J1, J2, *latt));
