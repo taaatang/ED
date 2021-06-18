c9af86ab629fb98f65b5485f249603024f839b04
taaatang <taaatang@outlook.com>
2021-06-18 09:13:20 -0700

    BICG_Test Jacobi preconditioner

--- Test/BiCGSTAB_Test.cpp
+++ Test/BiCGSTAB_Test.cpp
@@ -16,8 +16,9 @@ int main( ) {
 	// BiCGSTAB(&eye, z, b.data(), x.data(), eye.nloc, &eye, iterCount, res);
 	// std::cout << "iter: " << iterCount << ", err: " << res << '\n' << "solution:\n" << x << '\n'; 
 
-	Basis B(dim);
-	SparseMatrix<cdouble> A(&B, &B, 1, 0);
+	std::cout << "creat A matrix\n" << std::flush;
+	SparseMatrix<cdouble> A(dim, dim, 1, 0);
+	std::cout << "created A matrix\n" << std::flush;
 	cdouble a = 1.0;
 	cdouble c = 1.5;
 	MAP<cdouble> frow({{idx_t(0), a}, {idx_t(1), c}});
@@ -26,23 +27,25 @@ int main( ) {
 		MAP<cdouble> row({{i - 1, c}, {i, a}, {i + 1, c}});
 		A.pushRow(&row);
 	}
+	std::cout << "lrow\n" << std::flush;	
 	MAP<cdouble> lrow({{idx_t(dim - 2), c}, {idx_t(dim - 1), a}});
 	A.pushRow(&lrow);
+	std::cout << "lrow finished\n" << std::flush;
 	A.build();
 
+	std::cout << "get A diag\n" << std::flush;
 	std::vector<cdouble> diag;
 	A.getDiag(diag);
 	std::cout << "diag of A:\n" << diag << '\n' << std::flush; 
 
-	SparseMatrix<cdouble> Minv(&B, &B, 0, 1);
-	for (int i = 0; i < dim; ++i) {
-		Minv.putDiag(1.0 / (a + z), i);
-	}
-	Minv.created();
+	SparseMatrix<cdouble> Minv(dim, dim, 0, 1);
+	//! Fix: Matrix free, diagonal MxV
+	Minv.assignDiagShiftInv(diag, z);
+	// Minv.created();
 
 	scale(x.data(), cdouble(0.0), dim);
-	BiCGSTAB(&A, z, b.data(), x.data(), A.nloc, &eye, iterCount, res);
-	// BiCGSTAB(&A, z, b.data(), x.data(), A.nloc, &Minv, iterCount, res);
+	BiCGSTAB(&A, z, b.data(), x.data(), A.nloc, &Minv, iterCount, res);
+	// BiCGSTAB(&A, z, b.data(), x.data(), A.nloc, &eye, iterCount, res);
 	printLine();
 	std::cout << "iter: " << iterCount << ", err: " << res << '\n';
 	// std::cout << "solution:\n" << x << '\n';
