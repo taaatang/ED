14a7212770ff6e8485cd4b5344bd19704518fa29
taaatang <taaatang@outlook.com>
2021-08-03 20:59:46 -0700

    local operators passed simple test

---
+++ Operator/localOperators.hpp
@@ -0,0 +1,259 @@
+//
+// Created by tatang on 8/3/21.
+//
+
+#ifndef ED_LOCALOPERATORS_HPP
+#define ED_LOCALOPERATORS_HPP
+
+#include <optional>
+#include "Global/globalType.hpp"
+#include "Basis/Basis.hpp"
+#include "Utils/bitop.hpp"
+
+template<IsBasisType B>
+struct BasisVal {
+    B basis;
+    double val{1.0};
+    BasisVal() = delete;
+    BasisVal(B b) : basis(b) {}
+};
+
+template<IsBasisType B>
+using BVopt = std::optional<BasisVal<B>>;
+
+template<SPIN S>
+struct CPlus {
+    int pos{0};
+    explicit CPlus(int i = 0) : pos(i) {}
+    const CPlus& operator()(int i) {
+        pos = i;
+        return *this;
+    }
+};
+
+template<SPIN S>
+struct CMinus {
+    int pos{0};
+    explicit CMinus(int i = 0) : pos(i) { }
+    const CMinus& operator()(int i) {
+        pos = i;
+        return *this;
+    }
+};
+
+template<SPIN S>
+struct NCharge {
+    int pos{0};
+    explicit NCharge(int i = 0) : pos(i) { }
+    const NCharge& operator()(int i) {
+        pos = i;
+        return *this;
+    }
+};
+
+struct APlus {
+    int pos{0};
+    explicit APlus(int i = 0) : pos(i) { }
+    const APlus& operator()(int i) {
+        pos = i;
+        return *this;
+    }
+};
+
+struct AMinus {
+    int pos{0};
+    explicit AMinus(int i = 0) : pos(i) { }
+    const AMinus& operator()(int i) {
+        pos = i;
+        return *this;
+    }
+};
+
+struct NPhonon {
+    int pos{0};
+    explicit NPhonon(int i = 0) : pos(i) { }
+    const NPhonon& operator()(int i) {
+        pos = i;
+        return *this;
+    }
+};
+
+struct SPlus {
+    int pos{0};
+    explicit SPlus(int i = 0) : pos(i) { }
+    const SPlus& operator()(int i) {
+        pos = i;
+        return *this;
+    }
+};
+
+struct SMinus {
+    int pos{0};
+    explicit SMinus(int i = 0) : pos(i) { }
+    const SMinus& operator()(int i) {
+        pos = i;
+        return *this;
+    }
+};
+
+struct Sz {
+    int pos{0};
+    explicit Sz(int i = 0) : pos(i) { }
+    const Sz& operator()(int i) {
+        pos = i;
+        return *this;
+    }
+};
+
+template<SPIN S, ContainCharge B>
+BVopt<B> operator*(const CPlus<S>& cp, BVopt<B> bv) {
+    if (bv) {
+        idx_t* state;
+        if constexpr(S == SPIN::UP) {
+            state = &bv->basis.upState();
+        } else {
+            state = &bv->basis.dnState();
+        }
+        if(!bitTest(*state, cp.pos)) {
+            bitFlip(*state, cp.pos);
+            int counter = bitCount(*state, cp.pos);
+            if constexpr(S == SPIN::DOWN) {
+                counter += bitCount(bv->basis.upState(), B::getNSite());
+            }
+            int sgn = (counter % 2 == 0) ? 1 : -1;
+            bv->val *= sgn;
+        } else {
+            return std::nullopt;
+        }
+    }
+    return bv;
+}
+
+template<SPIN S, ContainCharge B>
+BVopt<B> operator*(const CMinus<S>& cm, BVopt<B> bv) {
+    if (bv) {
+        idx_t* state;
+        if constexpr(S == SPIN::UP) {
+            state = &bv->basis.upState();
+        } else {
+            state = &bv->basis.dnState();
+        }
+        if(bitTest(*state, cm.pos)) {
+            bitFlip(*state, cm.pos);
+            int counter = bitCount(*state, cm.pos);
+            if constexpr(S == SPIN::DOWN) {
+                counter += bitCount(bv->basis.upState(), B::getNSite());
+            }
+            int sgn = (counter % 2 == 0) ? 1 : -1;
+            bv->val *= sgn;
+        } else {
+            return std::nullopt;
+        }
+    }
+    return bv;
+}
+
+template<SPIN S, ContainCharge B>
+BVopt<B> operator*(const NCharge<S>& nch, BVopt<B> bv) {
+    if (bv) {
+        idx_t* state;
+        if constexpr(S == SPIN::UP) {
+            state = &bv->basis.upState();
+        } else {
+            state = &bv->basis.dnState();
+        }
+        if(bitTest(*state, nch.pos)) {
+            bv->val *= 1.0;
+        } else {
+            bv->val *= 0.0;
+        }
+    }
+    return bv;
+}
+
+template<ContainPhonon B>
+BVopt<B> operator*(const APlus& ap, BVopt<B> bv) {
+    if (bv) {
+        auto& state = bv->basis.phState();
+        if (state.at(ap.pos) < B::getMaxPhPerSite()) {
+            state[ap.pos] += 1;
+            bv->val *= std::sqrt(double(state[ap.pos]));
+            return bv;
+        }
+    }
+    return std::nullopt;
+}
+
+template<ContainPhonon B>
+BVopt<B> operator*(const AMinus& a, BVopt<B> bv) {
+    if (bv) {
+        auto& state = bv->basis.phState();
+        if (state.at(a.pos) > 0) {
+            bv->val *= std::sqrt(double(state[a.pos]));
+            state[a.pos] -= 1;
+            return bv;
+        }
+    }
+    return std::nullopt;
+}
+
+template<ContainPhonon B>
+BVopt<B> operator*(const NPhonon& n, BVopt<B> bv) {
+    if (bv) {
+        bv->val *= double(bv->basis.phState().at(n.pos));
+        return bv;
+    }
+    return std::nullopt;
+}
+
+template<ContainSpin B>
+BVopt<B> operator*(const SPlus& sp, BVopt<B> bv) {
+    if (bv) {
+        if constexpr (IsPureSpin<B>) {
+            auto& state = bv->basis.spinState();
+            if (!bitTest(state, sp.pos)) {
+                bitFlip(state, sp.pos);
+                return bv;
+            }
+        } else if constexpr (ContainCharge<B>){
+            return CPlus<SPIN::UP>(sp.pos) * (CMinus<SPIN::DOWN>(sp.pos) * bv);
+        }
+    }
+    return std::nullopt;
+}
+
+template<ContainSpin B>
+BVopt<B> operator*(const SMinus& sm, BVopt<B> bv) {
+    if (bv) {
+        if constexpr (IsPureSpin<B>) {
+            auto& state = bv->basis.spinState();
+            if (bitTest(state, sm.pos)) {
+                bitFlip(state, sm.pos);
+                return bv;
+            }
+        } else if constexpr (ContainCharge<B>){
+            return CPlus<SPIN::DOWN>(sm.pos) * (CMinus<SPIN::UP>(sm.pos) * bv);
+        }
+    }
+    return std::nullopt;
+}
+
+template<ContainSpin B>
+BVopt<B> operator*(const Sz& sz, BVopt<B> bv) {
+    if (bv) {
+        if constexpr (IsPureSpin<B>) {
+            auto& state = bv->basis.spinState();
+            bv->val *= bitTest(state, sz.pos) ? 0.5 : -0.5;
+            return bv;
+        } else if constexpr (ContainCharge<B>){
+            auto& up = bv->basis.upState();
+            auto& dn = bv->basis.dnState();
+            if (bitTest(up, sz.pos) != bitTest(dn, sz.pos)) {
+                bv->val *= bitTest(up, sz.pos) ? 0.5 : -0.5;
+                return bv;
+            }
+        }
+    }
+    return std::nullopt;
+}
+#endif //ED_LOCALOPERATORS_HPP
--- Test/basisState.cpp
+++ Test/basisState.cpp
@@ -3,13 +3,14 @@
 //
 
 #include "Basis/BasisState.hpp"
-#include "Basis/BasisBase.hpp"
+#include "Basis/Basis.hpp"
+#include "Operator/localOperators.hpp"
 
 int main() {
     int nSite = 4;
-    int nu = 2;
+    int nu = 1;
     int nd = 2;
-    int nPho = 1;
+    int nPho = 3;
 //    SpinBasis::configure(nSite, nu, nd, nPho);
 //    SpinBasis state;
 
@@ -35,11 +36,26 @@ int main() {
     b.construct();
     std::cout << b << std::endl;
 
-    // test search
-    for (idx_t i = 0; i < b.getSubDim(); ++i) {
-        auto idxOpt = b.search(b.get(i));
-        assert_msg(idxOpt != std::nullopt, "not found!");
-        std::cout << "found " << *idxOpt << std::endl;
+//    CPlus<SPIN::UP> cpu;
+//    CPlus<SPIN::DOWN> cpd;
+//    CMinus<SPIN::UP> cmu;
+//    CMinus<SPIN::DOWN> cmd;
+//    NCharge<SPIN::UP> nelu;
+//    NCharge<SPIN::DOWN> neld;
+
+    auto bvi = BVopt<ElectronPhononBasis>{b.get(760)};
+    auto bvf = Sz(2) * bvi;
+    std::cout << "initial: " << bvi->basis << ' ' << bvi->val << std::endl;
+    if (bvf) {
+        std::cout << "final  : " << bvf->basis << ' ' << bvf->val << std::endl;
     }
+    // test search
+//    for (idx_t i = 0; i < b.getSubDim(); ++i) {
+//        auto idxOpt = b.search(b.get(i));
+//        assert_msg(idxOpt != std::nullopt, "not found!");
+//        std::cout << "found " << *idxOpt << std::endl;
+//    }
+
+
     return 0;
 }
