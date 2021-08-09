63d5e742c23a7a62052bcbf7dd66f6a817fb288e
taaatang <taaatang@outlook.com>
2021-08-09 09:22:09 -0700

    remove trival destructor

--- Pulse/pulse.hpp
+++ Pulse/pulse.hpp
@@ -25,35 +25,51 @@ double GaussPulseDerivative(double t, void* params);
 
 double GaussPulseDerivative2(double t, void* params);
 
-class Pulse{
 /*
     Gaussian shape elextric field with frequency w.
     return vector potential A(t) = \int_{ti}^{t} E(t).
     unit: hbar = 1, eV = 1, c = 1, e = 1
           tu = hbar/eV, Lu = c*tu, Eu = eV/e/Lu
 */
+class Pulse{
 public:
     Pulse(double w_ = 0.0/*eV*/, double sigma = 50.0/*fs*/, double dt_ = 0.01/*tu*/, double duration_ = 10.0/* in unit of sqrt(2)*sigma */, bool useA_ = true);
-    ~Pulse(){};
 
     double getE(int stepIdx) const;
+
     double getw( ) const { return params[2]; }
+
     double getFluence( ) const { return Fluence; }
+
     double computeFluence( ) const;
+
     double getA( ) const;
+
     double getA(double t_) const;
-    double getAa( ) const { return a * getA(); } // return A*a
+
+    // return A * a
+    double getAa( ) const { return a * getA(); }
+
     Vec3d getPol( ) const { return pol; }
+
     double getdt( ) const { return dt; }
+
     int getCount( ) const { return count; }
+
     int getStepNum( ) const { return numSteps; }
 
     void setAmp(double A0) { params[0] = A0; }
+
     void setWidth(double sigma_, double duration_);
+
     void setFreq(double w) { params[2] = w; }
+
     void setPhase(double phase) { params[3] = phase * 2.0 * PI; }
+
     void setFluence(double Flu /* mJ/cm^2 */);
+
     void setPol(Vec3d pol) { this->pol = pol;}
+
     void setFuncPara( );
 
     void seta(double a_/*nm*/) { a = a_/Lu; }
@@ -70,25 +86,51 @@ public:
 
 private:
     bool useA;
-    mutable double E, A;
 
-    double Eu, Au; // electric field unit
-    Vec3d pol; // polarization in x-y-z coordinates
-    double params[4]; // params = {E0, sigma, w, phase}, sigma = sqrt(2)*width, where I(width) = I(0)/e
+    mutable double E;
+
+    mutable double A;
+
+    // electric field unit
+    double Eu;
+
+    double Au; 
+
+    // polarization in x-y-z coordinates
+    Vec3d pol;
+
+    // params = {E0, sigma, w, phase}, sigma = sqrt(2)*width, where I(width) = I(0)/e
+    double params[4];
+
     double Fluence;
 
-    double Lu, a;
+    double Lu;
+
+    double a;
 
     double dt;
+
     int count{0};
+
     int numSteps{0};
+
     mutable int total{1};
-    double tu; // time unit: hbar/eV, fs
-    double ti, tc, tf; 
+
+    // time unit: hbar/eV, fs
+    double tu;
+
+    double ti;
+
+    double tc;
+
+    double tf;
+
     mutable double t;
 
     gsl_function FuncE;
+
     gsl_function FuncE2;
+
     double epsabs{1e-8}, epsrel{1e-8};
 };
 
