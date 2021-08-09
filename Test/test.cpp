a62e3046022726e5a8921cdbbd662a5d634b4a31
taaatang <taaatang@outlook.com>
2021-08-09 14:49:19 -0700

    rename test src

--- Test/test.cpp
+++ Test/test.cpp
@@ -61,7 +61,7 @@ int main(int argc, char *argv[]) {
     H.pushLinks({tCuPx, tPxCu, gCu, gPx}).pushV({ORBITAL::Dx2y2}, Vd).pushV({ORBITAL::Px}, Vp).pushU({ORBITAL::Dx2y2}, Udd).pushU({ORBITAL::Px}, Upp).transform().construct();
     H.printLinks();
     H.diag();
-//    H.print("Hamiltonian info");
+    // H.print("Hamiltonian info", std::cout, false);
 //    std::cout << "eval: " << H.getEval() << std::endl;
 
     Nocc<Basis_t> nocc(&latt, &b, &b);
@@ -106,44 +106,47 @@ int main(int argc, char *argv[]) {
         }
     }
     
-    Pulse pulse;
-    setpulse(pulsePara, pulse);
-    H.setPeierls(&pulse);
-    H.construct();
-    TimeEvolver<dataType> Tevol(H.getEvec(), &H, timeKD);
-    pulse.progressBar(20);
-    timer.tik();
-    while (H.next()) {
-        Tevol.evolve(pulse.getdt());
-        nocc.count(Tevol.getVec());
-        time += pulse.getdt();
-        timePoints.push_back(time);
-        if (isMaster) {
-            pulse.progress();
+    if (opt(measurePara, "pump")) {
+            Pulse pulse;
+            setpulse(pulsePara, pulse);
+            H.setPeierls(&pulse);
+            H.construct();
+            TimeEvolver<dataType> Tevol(H.getEvec(), &H, timeKD);
+            pulse.progressBar(20);
+            timer.tik();
+            while (H.next()) {
+                Tevol.evolve(pulse.getdt());
+                nocc.count(Tevol.getVec());
+                time += pulse.getdt();
+                timePoints.push_back(time);
+                if (isMaster) {
+                    pulse.progress();
+                }
+            }
+            timer.print("pump time evolution");
+            H.turnOffPulse();
+
+            if (opt(measurePara, "neqSkw")) {
+            double dt1 = *measurePara.template get<double>("dt1");
+            int steps1 = *measurePara.template get<int>("steps1");
+            double dt2 = *measurePara.template get<double>("dt2");
+            int steps2 = *measurePara.template get<int>("steps2");
+            for (int i = 0; i < steps1; ++i) {
+                nocc.count(Tevol.getVec());
+                time += dt1;
+                timePoints.push_back(time);
+                int k = 2;
+                // for (int k = 0; k < szk.size(); ++k) {
+                timer.tik();
+                doubleTimeCorrelator(szk[(nSite - k) % nSite].get(), szk[k].get(), &H, Tevol.getVec(), timeKD, dt2, steps2, path.NeqSkwDir + "Cu/k" + tostr(k) + "/t1_" + tostr(i), isMaster);
+                // }
+                Tevol.evolve(dt1);
+                timer.print("szk(t1,t2)");
+            }
         }
+        nocc.save(path.NeqOccDir);
+        save<double>(timePoints.data(), int(timePoints.size()), path.NeqOccDir + "/timePoints");
     }
-    timer.print("pump time evolution");
-    H.turnOffPulse();
-
-
-    double dt1 = *measurePara.template get<double>("dt1");
-    int steps1 = *measurePara.template get<int>("steps1");
-    double dt2 = *measurePara.template get<double>("dt2");
-    int steps2 = *measurePara.template get<int>("steps2");
-    for (int i = 0; i < steps1; ++i) {
-        nocc.count(Tevol.getVec());
-        time += dt1;
-        timePoints.push_back(time);
-        int k = 2;
-        // for (int k = 0; k < szk.size(); ++k) {
-        timer.tik();
-        doubleTimeCorrelator(szk[(nSite - k) % nSite].get(), szk[k].get(), &H, Tevol.getVec(), timeKD, dt2, steps2, path.NeqSkwDir + "Cu/k" + tostr(k) + "/t1_" + tostr(i), isMaster);
-        // }
-        Tevol.evolve(dt1);
-        timer.print("szk(t1,t2)");
-    }
-    nocc.save(path.NeqOccDir);
-    save<double>(timePoints.data(), int(timePoints.size()), path.NeqOccDir + "/timePoints");
     
     MPI_Finalize();
     return 0;
